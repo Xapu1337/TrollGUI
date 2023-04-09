a89c8f4938b164db1fb38ba46bcdbdc198aaeb5b
Xapu1337 <xapursplaz@gmail.com>
2023-04-09 20:54:42 +0200

    V4 Start! full recode soon

---
+++ src/main/java/me/xapu1337/recodes/trollgui/inventories/PlayerSelectorInventory.java
@@ -0,0 +1,2 @@
+package me.xapu1337.recodes.trollgui.inventories;public class PlayerSelectorInventory {
+}
---
+++ src/main/java/me/xapu1337/recodes/trollgui/utilities/DebuggingUtil.java
@@ -0,0 +1,2 @@
+package me.xapu1337.recodes.trollgui.utilities;public class DebuggingUtil {
+}
